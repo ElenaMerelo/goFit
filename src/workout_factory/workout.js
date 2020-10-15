ea34ace4016459622041bac747cd25b3da465087
ElenaMerelo <nenamer@gmail.com>
2020-10-15 20:28:05 +0200

    desarrolla mas el esqueleto

--- src/workout_factory/workout.js
+++ src/workout_factory/workout.js
@@ -2,8 +2,9 @@ import Exercise from "./exercise.js";
 
 
 export default class Workout {
-    constructor(duration, intensity, muscleGroup, discipline, exercises){
-        this.duration = duration;
+    constructor(total_duration, ex_duration, intensity, muscleGroup, discipline, exercises){
+        this.total_duration = total_duration;
+        this.ex_duration = ex_duration;
         this.intensity = intensity;
         this.muscleGroup = muscleGroup;
         this.discipline = discipline;
--- src/workout_factory/workout_factory.js
+++ src/workout_factory/workout_factory.js
@@ -1,12 +1,38 @@
 import Workout from "./workout.js"
+import exercises from "../tests/exercises.json";
+
 export default class WorkoutFactory {
-    constructor(path_file) {}
+    constructor(exercises) {
+        this.exercises= exercises;
+    }
 
 
     createFrom({discipline, intensity, muscleGroup}) {}
-    createFromDiscipline(discipline) {}
-    createFromIntensity(intensity) {}
-    createFromMuscleGroup(muscleGroup) {}
-    readDataFile(file) {}
+    createFromDiscipline(discipline) {
+        var filtered= [];
+        for(var i= 0; i< exercises.lenght; i++){
+            if(exercises[i].discipline == discipline)
+                filtered.push(exercises[i]);
+        }
+        return filtered;
+    }
+
+    createFromIntensity(intensity) {
+        var filtered= [];
+        for(var i= 0; i< exercises.lenght; i++){
+            if(exercises[i].intensity == intensity)
+                filtered.push(exercises[i]);
+        }
+        return filtered;
+    }
+
+    createFromMuscleGroup(muscleGroup) {
+        var filtered= [];
+        for(var i= 0; i< exercises.lenght; i++){
+            if(exercises[i].muscleGroup == muscleGroup)
+                filtered.push(exercises[i]);
+        }
+        return filtered;
+    }
  }
 
