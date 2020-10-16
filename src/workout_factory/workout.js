76f743279578ac3f9a821b8814b7a4a58195844f
ElenaMerelo <nenamer@gmail.com>
2020-10-16 18:29:08 +0200

    completa clase workout (#22)

--- src/workout_factory/workout.js
+++ src/workout_factory/workout.js
@@ -1,13 +1,19 @@
 import Exercise from "./exercise.js";
 
-
 export default class Workout {
-    constructor(total_duration, ex_duration, intensity, muscleGroup, discipline, exercises){
-        this.total_duration = total_duration;
-        this.ex_duration = ex_duration;
-        this.intensity = intensity;
-        this.muscleGroup = muscleGroup;
-        this.discipline = discipline;
-        this.exercises = exercises;
-    }
-}   
+  constructor(
+    total_duration,
+    ex_duration,
+    intensity,
+    muscleGroup,
+    disciplines,
+    exercises
+  ) {
+    this.total_duration = total_duration;
+    this.ex_duration = ex_duration;
+    this.intensity = intensity;
+    this.muscleGroup = muscleGroup;
+    this.disciplines = disciplines;
+    this.exercises = exercises;
+  }
+}
