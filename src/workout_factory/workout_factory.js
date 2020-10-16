98cf00ea38d945cfe59a2d119cc1db97a84bc323
ElenaMerelo <nenamer@gmail.com>
2020-10-16 18:30:36 +0200

    avanza workout factory (#21)

--- src/workout_factory/workout_factory.js
+++ src/workout_factory/workout_factory.js
@@ -1,34 +1,32 @@
-import Workout from "./workout.js"
-import exercises from "../tests/exercises.json";
-import Exercise, {Discipline, Intensity, MuscleGroup} from "./exercise.js";
+import Workout from "./workout.js";
+import exercises from "../tests/exercises_data.js";
+import Exercise, { Discipline, Intensity, MuscleGroup } from "./exercise.js";
 
 export default class WorkoutFactory {
-    constructor(exercises) {
-        this.exercises= exercises;
+  constructor(exercises) {
+    this.exercises = exercises;
+  }
+
+  createFrom({ discipline, intensity, muscleGroup }) {
+    // this.createFrom({discipline: disciplines.PILATES, muscleGroups.UPPERBODY})
+    let filtered = this.exercises;
+    if (discipline !== undefined)
+      filtered = this.filterBy(filtered, "discipline", discipline);
+
+    if (intensity !== undefined)
+      filtered = this.filterBy(filtered, "intensity", intensity);
+
+    if (muscleGroup !== undefined)
+      filtered = this.filterBy(filtered, "muscleGroup", muscleGroup);
+
+    return filtered;
+  }
+
+  filterBy(exercises, attr, value) {
+    let filtered = [];
+    for (let i = 0; i < exercises.lenght; i++) {
+      if (exercises[i][attr] == value) filtered.push(exercises[i]);
     }
-
-    createFrom({discipline, intensity, muscleGroup}) {
-        // this.createFrom({discipline: disciplines.PILATES, muscleGroups.UPPERBODY})
-        let filtered= this.exercises;
-        if(discipline !== undefined)
-            filtered=this.filterBy(filtered, "discipline", discipline);
-
-        if(intensity !== undefined)
-            filtered=this.filterBy(filtered, "intensity", intensity);
-        
-        if(muscleGroup !== undefined)
-            filtered=this.filterBy(filtered, "muscleGroup", muscleGroup);
-
-        return filtered;
-    }
-
-    filterBy(exercises, attr, value) {
-        let filtered= [];
-        for(let i= 0; i< exercises.lenght; i++){
-            if(exercises[i][attr] == value)
-                filtered.push(exercises[i]);
-        }
-        return filtered;
-    }
- }
-
+    return filtered;
+  }
+}
