ad3f30cf1bff9a39cc6ed32b3e9b5c80d17f8a96
ElenaMerelo <nenamer@gmail.com>
2020-11-26 18:27:36 +0100

    Crea funcion en netlify (#41), close #43

---
+++ functions/gofit.js
@@ -0,0 +1,29 @@
+const telegraf = require("telegraf");
+const fetch = require("node-fetch");
+
+const API_URL = process.env.API_URL;
+const bot = new telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+bot.start((ctx) => ctx.reply(process.uptime()));
+
+bot.hears(/\/workout (.+)/, async (ctx) => {
+  const [
+    command,
+    intensity,
+    muscleGroup,
+    disciplines,
+    duration,
+  ] = ctx.message.text.split(" ");
+  const res = await (
+    await fetch(
+      API_URL +
+        `/workout?intensity=${intensity}&muscle_group=${muscleGroup}&disciplines=${disciplines}&duration=${duration}`
+    )
+  ).json();
+  bot.telegram.sendMessage(ctx.chat.id, res);
+});
+
+exports.handler = async (event, ctx, callback) => {
+  await bot.handleUpdate(JSON.parse(event.body));
+  return callback(null, { statusCode: 200 });
+};
