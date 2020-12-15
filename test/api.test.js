63bbda102157b3d1d5732503470be46ad68b9e19
ElenaMerelo <nenamer@gmail.com>
2020-12-15 18:52:36 +0100

    Fix tests

--- test/api.test.js
+++ test/api.test.js
@@ -1,18 +1,20 @@
-const supertest = require("supertest");
+const request = require("supertest");
 const expect = require("chai").expect;
 const app = require("../src/workout_factory/app.js");
 
-const request = supertest(app);
-
 describe("GET /api/workout/discipline/:discipline", () => {
   it("Returns 200", async function () {
-    const response = await request.get("/api/workout/discipline/kickboxing");
+    const response = await request(app).get(
+      "/api/workout/discipline/kickboxing"
+    );
     expect(response.status).to.equal(200);
     expect(response.body.length).to.equal(1);
   });
 
   it("Returns correct exercise", async function () {
-    const response = await request.get("/api/workout/discipline/kickboxing");
+    const response = await request(app).get(
+      "/api/workout/discipline/kickboxing"
+    );
     const exercise = response.body[0];
     expect(exercise.name).to.equal("front kick");
     expect(exercise._muscleGroup).to.equal("lower_body");
@@ -21,7 +23,7 @@ describe("GET /api/workout/discipline/:discipline", () => {
   });
 
   it("Returns 400", async function () {
-    const response = await request.get(
+    const response = await request(app).get(
       "/api/workout/discipline/invalid_discipline"
     );
     expect(response.status).to.equal(400);
@@ -30,13 +32,17 @@ describe("GET /api/workout/discipline/:discipline", () => {
 
 describe("GET /api/workout/muscle_group/:muscle_group", () => {
   it("Returns 200", async function () {
-    const response = await request.get("/api/workout/muscle_group/total_body");
+    const response = await request(app).get(
+      "/api/workout/muscle_group/total_body"
+    );
     expect(response.status).to.equal(200);
     expect(response.body.length).to.equal(1);
   });
 
   it("Returns correct exercise", async function () {
-    const response = await request.get("/api/workout/muscle_group/total_body");
+    const response = await request(app).get(
+      "/api/workout/muscle_group/total_body"
+    );
     const exercise = response.body[0];
     expect(exercise.name).to.equal("burpee");
     expect(exercise._muscleGroup).to.equal("total_body");
@@ -45,20 +51,22 @@ describe("GET /api/workout/muscle_group/:muscle_group", () => {
   });
 
   it("Returns 400", async function () {
-    const response = await request.get("/api/workout/muscle_group/invalid");
+    const response = await request(app).get(
+      "/api/workout/muscle_group/invalid"
+    );
     expect(response.status).to.equal(400);
   });
 });
 
 describe("GET /api/workout/intensity/:intensity", () => {
   it("Returns 200", async function () {
-    const response = await request.get("/api/workout/intensity/low");
+    const response = await request(app).get("/api/workout/intensity/low");
     expect(response.status).to.equal(200);
     expect(response.body.length).to.equal(3);
   });
 
   it("Returns correct exercise", async function () {
-    const response = await request.get("/api/workout/intensity/low");
+    const response = await request(app).get("/api/workout/intensity/low");
     const exercise = response.body[0];
     expect(exercise.name).to.equal("downDog");
     expect(exercise._muscleGroup).to.equal("upper_body");
@@ -72,7 +80,7 @@ describe("GET /api/workout/intensity/:intensity", () => {
   });
 
   it("Returns 400", async function () {
-    const response = await request.get("/api/workout/intensity/invalid");
+    const response = await request(app).get("/api/workout/intensity/invalid");
     expect(response.status).to.equal(400);
   });
 });
